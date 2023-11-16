7a19f746cf1a44c23b82424a2efe1bb4d4f8cdf9
Marcos Carvalho <marc.carvalho98@gmail.com>
2023-11-16 15:55:09 -0300

    Alguns testes sobre a linguagem

---
+++ outrosTestes.py
@@ -0,0 +1,91 @@
+# # dicionario = {}
+# # c = "chave"
+# # n = "valor"
+# # dicionario[c] = []
+# # dicionario[c].append(n)
+# # print(dicionario)
+# # dicionario[c].append("valor 2")
+# # print(dicionario)
+# # if "azul"  not in dicionario.keys():
+# #     dicionario["azul"] = []
+# #     dicionario["azul"].append("amarelo")
+# # if "amarelo" not in dicionario["azul"]:
+# #     dicionario["azul"].append("amarelo")
+# # print(dicionario)
+# # print("ok")
+
+# # lista = ["a","b","c","d"]
+# # palavra = "-".join(lista)
+# # print(palavra)
+
+# # dicionario = {"a":[1,2,3],"b":[4,5,6,],"c":[7,8,9]}
+# # for i in dicionario.keys():
+# #     print("keys")
+# #     print(i)
+# # for i in dicionario.values():
+# #     print("values")
+# #     print(i)
+# # lista = ["a","b","c","d"]
+# # lista[1] = 10
+# # print(lista)
+# # palavra = "verde"
+# # palavra[3] = "k"
+
+# # dicionario_geral = {
+# #     "pessoa1": {'posição':[1,2],'valor':10,'movimentos':'abcsnedasd'},
+# # 'pessoa2':{'posição':[4,6],'valor':19,'movimentos':'asuidhasid'}
+# # }
+
+# # print(dicionario_geral)
+
+# # Função para calcular o valor do tesouro encontrado por um caçador
+# # Função para calcular o valor do tesouro encontrado por um caçador
+# def calcular_valor_tesouro(mapa, linha_inicial, coluna_inicial, movimentos):
+#     valor_tesouro = 0
+
+#     for movimento in movimentos:
+#         if movimento == 'N':
+#             linha_inicial -= 1
+#         elif movimento == 'S':
+#             linha_inicial += 1
+#         elif movimento == 'O':
+#             coluna_inicial -= 1
+#         elif movimento == 'L':
+#             coluna_inicial += 1
+
+#         # Verifica se a posição atual contém um tesouro e atualiza o valor do tesouro
+#         if 0 <= linha_inicial < len(mapa) and 0 <= coluna_inicial < len(mapa[0]):
+#             valor_tesouro += mapa[linha_inicial][coluna_inicial]
+#             mapa[linha_inicial][coluna_inicial] = 0
+
+#     return valor_tesouro
+
+# # Função principal
+# def main():
+#     resultados = []
+
+#     while True:
+#         try:
+#             n, m = map(int, input().split())
+#             mapa = [list(map(int, input().split())) for _ in range(n)]
+#             q = int(input())
+
+#             for cacador in range(1, q + 1):
+#                 linha_inicial, coluna_inicial = map(int, input().split())
+#                 movimentos = input().strip()  # Remova espaços em branco no início e no final da string
+#                 valor_tesouro = calcular_valor_tesouro(mapa, linha_inicial, coluna_inicial, movimentos)
+#                 resultados.append(f"Caçador C{cacador}: T{valor_tesouro}")
+
+#         except EOFError:
+#             break
+
+#     # Imprima todos os resultados ao final
+#     for resultado in resultados:
+#         print(resultado)
+
+# if __name__ == "__main__":
+#     main()
+# ruas_horizontais = [int(i) for i in input().split()]
+# print(ruas_horizontais)
+# lista = [int(i) for i in range(7,10)]
+# print(lista)
--- testes2.py
+++ testes2.py
@@ -1,5 +1,122 @@
-dicionario = {}
-chave = input()
-valor = input()
-dicionario["chave"].append(valor)
-print(dicionario.values)
+# dicionario = {}
+# print(type(dicionario))
+# localizacao = {
+
+#     "Lat": -22.837352,
+#     "Lon": -47.288282
+# }
+
+# print(localizacao)
+# dicionario1 = dict({})
+
+# dicionario2 = {
+#     1.2: True,
+#     123: "Um Dois Três",
+#     "cat" : "dog",
+#     ("X","Y"): (1,3,4)
+# }
+# print(dicionario2)
+
+# dicionario3 = {
+#     "Z":1,
+#     "A":2,
+#     "C":3
+# }
+# print(dicionario3)
+# print(dicionario3["A"])
+
+# dicionario4 = {
+#     "Nome":"Marcos",
+#     "Idade":24,
+#     "Profissão":"Estatistico"
+# }
+# print("Nome" in dicionario4)
+# print(dicionario4.get("Nome"))
+# print(len(dicionario4))
+# dicionario4["Cidade"] = "Campinas"
+# print(dicionario4)
+
+# dicionario4["Cidade"] = "São Paulo"
+# print(dicionario4)
+
+# valor_removido = dicionario4.pop("Cidade") # a declaração .del tbm remove mas não retorna o valor removido, apenas deleta
+# print(dicionario4)                          
+# print(valor_removido)
+# print()
+# valor_removido = dicionario4.popitem() # remove ultimo par de chave,valor e retorno eles como uma tupla
+# print(dicionario4)                          
+# print(valor_removido)
+
+# dic01 = { 
+#     "A" : "Amora",
+#     "B" : "Boba",
+#     "C" : "Cachorro"
+#     }
+# dic02 = { 
+#     "C": "Cachorro 2",
+#     "D" : "Doido",
+#     "E" : "Estou",
+#     "F" : "Facil"
+#     }
+
+# dic01.update(dic02)
+# print(dic01)
+# print(dic01.keys())
+# print(list(dic01.keys()))
+# print(dic01.values())
+# print(list(dic01.values()))
+# print(dic01.items()) #gera uma uma lista de tuplas
+
+# for letras in dic01.keys():
+#     print("Letras:",letras)
+
+# for frutas in dic01.values():
+#     print("Fruta:",frutas)
+
+# for (letras,frutas) in dic01.items():
+#     print("Fruta com Letra", letras, ":",frutas)
+
+# dic03 = {"Nome":"Joao", "Idade":20}
+# #print("03",dic03)
+# #dic04 = dic03
+# #print("04",dic04)
+# #dic03["Nome"] = "Maria"
+# #print("04mod",dic04)
+# #print("03   ",dic03)
+# ''' "copiar" um dicionario desta forma n é eficaz pois as variaveis estão ligadas aos objetos,
+# então uma alteração no dicionario copiado irá alterar o original tbm'''
+
+# dic03copy = dic03.copy()
+# print("03 copy",dic03copy)
+# dic03copy["Nome"] = "Lara"
+# print("03 copy",dic03copy)
+# print("03",dic03) #com metodo .copy() o dicionario oginal n é alterado
+
+# lista_chaves = ["Alice", "Carlos", "Lara"]
+# lista_valores = ["Camisa", "Blusa", "Vestido"]
+# pessoas = dict(zip(lista_chaves,lista_valores))
+# print(pessoas)
+
+# pessoas = {}
+# n = int(input("Qtd de pessoas: "))
+
+# for i in range(n):
+#     nome = input("nome: ")
+#     idade = int(input("idade: " ))
+#     sexo = input("Sexo: ")
+#     pessoas[nome] = [idade, sexo]
+# print(pessoas)
+# print(pessoas["Lara"][1])
+
+
+
+
+
+a = 10
+b = 20
+c= 0
+
+c = b
+b = a
+a = c
+print(a, b)
